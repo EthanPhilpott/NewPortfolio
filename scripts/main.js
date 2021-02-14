42832046638d10621e23fbc80a83e9fe2d5b4027
Ethan Philpott <ephilpott2004@gmail.com>
2021-02-13 22:40:30 -0700

    Made the opening

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let RNG_POSSIBLE_COLORS = ["#EF476F", "#FFD166", "#06D6A0", "#118AB2"];
+
+$(".rng-color").css(
+    "color",
+    RNG_POSSIBLE_COLORS[Math.floor(Math.random() * RNG_POSSIBLE_COLORS.length)]
+);
