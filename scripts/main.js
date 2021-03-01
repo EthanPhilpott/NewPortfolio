a7052d97f7c5b5a6b0a6ec732f15e19a6870cf34
Ethan Philpott <ephilpott2004@gmail.com>
2021-02-28 23:30:32 -0700

    redid the design for the opeing mandala

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-let RNG_POSSIBLE_COLORS = ["#EF476F", "#FFD166", "#06D6A0", "#118AB2"];
-
-$(".rng-color").css(
-    "color",
-    RNG_POSSIBLE_COLORS[Math.floor(Math.random() * RNG_POSSIBLE_COLORS.length)]
-);
